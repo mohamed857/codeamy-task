7391645d579e0adf19d8c7390cdf60c00bf2fb00
mohamed857 <mohamed857>
2024-07-18 01:57:35 +0300

    first commit

--- src/app/task/tasks/tasks.component.ts
+++ src/app/task/tasks/tasks.component.ts
@@ -12,12 +12,14 @@ import { CommonModule, DatePipe, UpperCasePipe } from '@angular/common';
 export class TasksComponent {
 // @Input({required:true}) task!:Task;
 completed: boolean = false;
+completeText:string = 'complete';
 task = input<Task>();
 @Output() complete= new EventEmitter<string>()
 @Output() update= new EventEmitter<Task>()
 completeTask = ()=>{
   this.complete.emit(this.task()!.id)
   this.completed = true;
+  this.completeText='completed';
 }
 updateTask = ()=>{
   this.update.emit(this.task())
